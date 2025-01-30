ad25d419a2cf292748b101a1201c1672907c1785
Admin <Admin@TNSDC-UD33JSI0034410A6C70700>
2025-01-30 16:39:28 +0530

    added classes and object package

---
+++ src/Course/ClassesAndObject.java
@@ -0,0 +1,24 @@
+package Course;
+
+import java.util.*;
+
+class user{
+	String name,dept,yop;
+	int id;
+}
+
+class book{
+	String name,author,genre,availableCopies,total_no_of_copies;
+	int id;
+}
+
+class 
+
+public class ClassesAndObject {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
