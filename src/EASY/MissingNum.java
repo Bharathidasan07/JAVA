bc5e73b317029b93bf3645c384daf9bfda3df0f2
Admin <Admin@TNSDC-UD33JSI0034410A6C70700>
2025-02-03 12:08:27 +0530

    Added two problems Missing Number and Maximum SubArray(Still Incomplete)

---
+++ src/EASY/MissingNum.java
@@ -0,0 +1,33 @@
+package EASY;
+
+import java.util.*;
+
+public class MissingNum {
+
+	public static void main(String[] args) {
+		int[] nums = {9,6,4,2,3,5,7,0,1};
+		Set<Integer> HS = new HashSet<>();
+		int n = nums.length,mn,Asum=0,Nsum;
+		
+		//In this brute force approach we can find the missing sum but time complexity is 6ms
+		// It can be reduced to 0ms that is given below
+//		for(int i=0;i<n;i++) {
+//			HS.add(nums[i]);
+//		}
+//		System.out.println(HS.toString());
+//		for(int i=0;i<=n;i++) {
+//			if(!HS.contains(i)) {
+//				System.out.println(i);
+//			}
+//		}
+		
+		// total sum - given array sum
+		// Eg: if n = 5 then sum = 15, given arr = {0,1,2,3,4} GAsum = 10 sum-GAsum= 5 MN = 5
+		Nsum = (n*(n+1))/2;
+		for(int e : nums) {
+			Asum+=e; 
+		}
+		System.out.println(Nsum-Asum);
+	}
+
+}
---
+++ src/medium/MaximumSubArray.java
@@ -0,0 +1,52 @@
+package medium;
+
+import java.util.*;
+
+public class MaximumSubArray {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,-1,-2,2,1,-2,1,4,-5,4};
+		int n = nums.length;
+		int p1=0,p2=n-1,max=Integer.MIN_VALUE;
+		
+		// This approach misses some potential subArrays 
+		/*
+		 * Wrong Answer
+			179 / 210 testcases passed
+			Input : nums = [1,2,-1,-2,2,1,-2,1,4,-5,4]
+			Output 
+			5
+			Expected
+			6
+		 */
+//		while(p1<p2) {
+//			int Csum = 0;
+//			for(int i=p1;i<p2;i++) {
+//				Csum+=nums[i];
+//			}
+//			if(Csum>max) {
+//				max=Csum;
+//			}
+//			if(nums[p1]<nums[p2]) {
+//				p1++;
+//			}else {
+//				p2--;
+//			}
+//		}
+		//{1,2,-1,-2,2,1,-2,1,4,-5,4}
+		//Brute force approach Still Incomplete
+		for(int i=0;i<n;i++) {
+			int Csum =nums[i];
+			for(int j=i+1;j<n;j++) {
+				Csum+=nums[j];
+				if(Csum>max) {
+					max=Csum;
+				}
+			}
+		}
+		
+		System.out.println(max);
+
+	}
+
+}
